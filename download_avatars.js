899799323fc6151f588891264182d2142415e57b
Anhad Gill <gillanhad23@gmail.com>
2018-03-20 02:47:18 +0000

    Added functionality that downloads all profile images.

--- download_avatars.js
+++ download_avatars.js
@@ -2,38 +2,33 @@ var request = require('request');
 var secret = require('./secrets');
 var fs = require('fs');
 
-// // console.log('Welcome to the GitHub Avatar Downloader!');
-// //Go ahead and add an Authorization header to your request call in getRepoContributors.
+// console.log('Welcome to the GitHub Avatar Downloader!');
+//Go ahead and add an Authorization header to your request call in getRepoContributors.
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var options = {
-//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//     json: true,
-//     headers: {
-//       'User-Agent': 'request',
-//       'Authentication': secret.GITHUB_TOKEN
-//     }
-//   };
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    json: true,
+    headers: {
+      'User-Agent': 'request',
+      'Authentication': secret.GITHUB_TOKEN
+    }
+  };
 
-//   request(options, function(err, res, body) {
-//     cb(err, body);
-//   });
-// }
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
 
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//     // console.log("Errors:", err);
-//     // console.log("Result:", result);
-//     result.forEach(element => {
-//         console.log(element.avatar_url)
-//     });
-//   });
-
-// What we need now is a generic function which is given two parameters:
-
-// url: A remote image URL to fetch
-// filePath: A local path for where to persist the file
-// The function will make a request to a given url, saving the resulting image file to a specified filePath.
-
+getRepoContributors("jquery", "jquery", function(err, result) {
+    // console.log("Errors:", err);
+    // console.log("Result:", result);
+    result.forEach(element => {
+        console.log(element.avatar_url);
+        downloadImageByURL(element.avatar_url, 'avatars/' + element.login)
+    });
+  });
+// passing in avatar_url from your callback function into downloadImageByURL.
 
 function downloadImageByURL(url, filePath) {
     request.get(url)
@@ -46,12 +41,12 @@ function downloadImageByURL(url, filePath) {
        .on('end', function (end) {
         console.log('Download complete.')
        })
-       .pipe(fs.createWriteStream(filePath));
+       .pipe(fs.createWriteStream(filePath + '.jpg'));
 }
 
 
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+//downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 
 
